1afad1964cf9d74afeb5df26e0d148d1443de681
Changjian Gao <gaochangjian@gmail.com>
2017-05-20 16:54:07 +0800

    Revert "Replace direct SetUseDirectWrites with option from 5.4.0"
    This reverts commit 13409fca3287a59af3a6e530444528ecd91b9634.

--- options.go
+++ options.go
@@ -702,11 +702,10 @@ func (opts *Options) SetUseDirectReads(value bool) {
 	C.rocksdb_options_set_use_direct_reads(opts.c, boolToChar(value))
 }
 
-// SetUseDirectIOForFlushAndCompaction enable/disable direct I/O mode (O_DIRECT) for both reads and writes in background flush and compactions
-// When true, new_table_reader_for_compaction_inputs is forced to true.
+// SetUseDirectWrites enable/disable direct I/O mode (O_DIRECT) for writes
 // Default: false
-func (opts *Options) SetUseDirectIOForFlushAndCompaction(value bool) {
-	C.rocksdb_options_set_use_direct_io_for_flush_and_compaction(opts.c, boolToChar(value))
+func (opts *Options) SetUseDirectWrites(value bool) {
+	C.rocksdb_options_set_use_direct_writes(opts.c, boolToChar(value))
 }
 
 // SetIsFdCloseOnExec enable/dsiable child process inherit open files.
